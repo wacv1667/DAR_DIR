874933881ffb698f3a3b30d9ef0563e62be0d78e
wacv1677 <143491224+wacv1677@users.noreply.github.com>
2023-08-31 09:23:22 +0200

    Update dataset_check.py

--- utils/dataset_check.py
+++ utils/dataset_check.py
@@ -1,1 +1,11 @@
+from glob import glob
 
+
+def dataset_check(base_dir: str = "./brain4cars_data/"):
+    for fold_num in range(5):
+        train_file_list = [x.split("/")[-1] for x in glob(f"{base_dir}/train/fold{fold_num}/**/*.avi") if "road_vids" not in x]
+        val_file_list = [x.split("/")[-1] for x in glob(f"{base_dir}/val/fold{fold_num}/**/*.avi") if "road_vids" not in x]
+        print(f"Number of overlapping files for fold {fold_num}: {len(set(train_file_list) & set(val_file_list))}")
+
+if __name__ == "__main__":
+    dataset_check()
